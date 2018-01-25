f93b176e053cbb8e9354fcdbd44c045230fe5249
Igor Bzovsky <igor77188@gmail.com>
2018-01-25 17:57:05 +0200

    Create database

---
+++ CommunalServices/Migrations/20180125134012_Initial.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CommunalServices.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    ParentId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Locations_Locations_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "Locations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MeasureUnit",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MeasureUnit", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Utilities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    MeasureUnitId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Utilities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Utilities_MeasureUnit_MeasureUnitId",
+                        column: x => x.MeasureUnitId,
+                        principalTable: "MeasureUnit",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Locations_ParentId",
+                table: "Locations",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Utilities_MeasureUnitId",
+                table: "Utilities",
+                column: "MeasureUnitId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "Utilities");
+
+            migrationBuilder.DropTable(
+                name: "MeasureUnit");
+        }
+    }
+}
--- CommunalServices/Persistance/ApplicationDbContext.cs
+++ CommunalServices/Persistance/ApplicationDbContext.cs
@@ -9,6 +9,9 @@ namespace CommunalServices.Persistance
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Utility> Utilities { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
