82c0f3e14273ae5a0a905a16585cdac8c011bf36
Igor Bzovsky <igor77188@gmail.com>
2018-01-25 15:38:39 +0200

    Initial model

---
+++ CommunalServices/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CommunalServices.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunalServices.Persistance
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- CommunalServices/Startup.cs
+++ CommunalServices/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using CommunalServices.Persistance;
 
 namespace CommunalServices
 {
@@ -22,6 +24,7 @@ namespace CommunalServices
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
         }
 
