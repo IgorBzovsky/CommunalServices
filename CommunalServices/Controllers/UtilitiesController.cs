67be18bb770fe83291d70b039aca11d7949b9b87
Igor Bzovsky <igor77188@gmail.com>
2018-01-26 19:20:48 +0200

    Get utilities API (initial)

---
+++ CommunalServices/Controllers/UtilitiesController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using CommunalServices.Controllers.Resources;
+using CommunalServices.Core.Models;
+using CommunalServices.Persistance;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunalServices.Controllers
+{
+    public class UtilitiesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public UtilitiesController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet("/api/utilities")]
+        public async Task<IEnumerable<UtilityResource>> GetUtilities()
+        {
+            var utilities = await _context.Utilities.ToListAsync();
+            return _mapper.Map<IEnumerable<Utility>, IEnumerable<UtilityResource>>(utilities);
+        }
+    }
+}
--- CommunalServices/Mapping/MappingProfile.cs
+++ CommunalServices/Mapping/MappingProfile.cs
@@ -13,6 +13,7 @@ namespace CommunalServices.Mapping
         public MappingProfile()
         {
             CreateMap<Location, LocationResource>();
+            CreateMap<Utility, UtilityResource>();
         }
     }
 }
